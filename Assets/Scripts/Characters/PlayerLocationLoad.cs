6bbdd99ca76c0efddab639023306186d336bc76d
Kroynix <57686467+Kroynix@users.noreply.github.com>
2021-12-09 19:20:42 -0500

    Parent Varient for Enemys, Save Load Button, Player Jumps only when grounded

--- Assets/Scripts/Characters/PlayerLocationLoad.cs
+++ Assets/Scripts/Characters/PlayerLocationLoad.cs
@@ -41,9 +41,13 @@ public class PlayerLocationLoad : MonoBehaviour
     private Vector3 Position;
     private int Direction;
 
+    [SerializeField]
+    private GameObject player;
+
 
     void Start()
     {
+        player = GameObject.Find("Wizard");
         SaveLocation(FindObjectOfType<PlayerBehaviourStateMachine>());
         Debug.Log("Saved");
     }
@@ -60,6 +64,26 @@ public class PlayerLocationLoad : MonoBehaviour
     {
         player.transform.position = instance.Position;
         player.direction = instance.Direction;
+    }
+
+
+    public void SavePlayerPosition()
+    {
+
+        PlayerPrefs.SetFloat("playerPosX", player.transform.position.x);
+        PlayerPrefs.SetFloat("playerPosY", player.transform.position.y);
+        PlayerPrefs.SetFloat("playerPosZ", player.transform.position.z);
+        PlayerPrefs.Save();
+
+    }
+
+    public void LoadPlayerPosition()
+    {
+        float playerPosX = PlayerPrefs.GetFloat("playerPosX");
+        float playerPosY = PlayerPrefs.GetFloat("playerPosY");
+        float playerPosZ = PlayerPrefs.GetFloat("playerPosZ");
+
+        player.transform.position = new Vector3(playerPosX,playerPosY,playerPosZ);
     }
 
 }
