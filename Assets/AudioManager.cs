0e79a27c706700013b8e61161e60ef4c09f0bde3
Kroynix <57686467+Kroynix@users.noreply.github.com>
2021-12-09 19:37:06 -0500

    AudioManager added

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    [SerializeField]
+	public Sound[] sounds;
+
+
+
+    private static AudioManager instance;
+    public static AudioManager Instance
+    {
+                get
+        {
+            if (instance == null)
+            {
+                instance = Instantiate(Resources.Load<AudioManager>("AudioManager"));
+                DontDestroyOnLoad(instance.gameObject);
+            }
+
+            return instance;
+        }
+    }
+
+
+
+	void Awake()
+	{
+        if (instance != null && instance != this)
+        {
+            Debug.Log("dest!");
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+		foreach (Sound s in sounds)
+		{
+			s.source = gameObject.AddComponent<AudioSource>();
+			s.source.clip = s.clip;
+			s.source.loop = s.loop;
+            s.source.volume = s.volume;
+		}
+	}
+
+	public void Play(string sound)
+	{
+		Sound s = Array.Find(sounds, item => item.name == sound);
+		if (s == null)
+		{
+			Debug.Log("Sound Clip not found");
+			return;
+		}
+		s.source.Play();
+	}
+
+}
---
+++ Assets/Scripts/Utility/Sound.cs
@@ -0,0 +1,22 @@
+using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound 
+{
+
+	public string name;
+
+	public AudioClip clip;
+
+	[Range(0f, 1f)]
+	public float volume = 1.0f;
+	[Range(.1f, 1f)]
+	public float pitch = 1f;
+	public bool loop = false;
+	
+
+	[HideInInspector]
+	public AudioSource source;
+
+}
